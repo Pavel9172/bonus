ba9c7c5c02c892e6bf1ff83f29ee4e35a30138ae
Pavel9172 <pavlo_k@mail.ru>
2022-03-18 22:23:14 +0500

    itog + comet

--- src/main.java
+++ src/main.java
@@ -1,7 +1,7 @@
 public class main {
     public static void main(String[] args) {
-        int balance = 100;
-        int deposit = 11000;
+        int balance = 100; // начальный баланс
+        int deposit = 11000; // пополнение счета
         int bonus = deposit / 100;
         int itog = balance + deposit + bonus;
 
